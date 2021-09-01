8205cc4b371fc8dcb6d50a30195e87d137296849
hwanheee <hwanheee2@naver.com>
2021-09-01 15:07:26 +0900

    0901김환희 테스트

---
+++ src/test/java/com/green/biz/SearchMaxTest.java
@@ -0,0 +1,22 @@
+package com.green.biz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SearchMaxTest {
+
+	@Test
+	public void test() {
+		
+		CalcUtil testSearchMax=new CalcUtil();
+		
+		int[]arr1=new int[] {1, 3, 4, 2};
+		int[]arr2=new int[] {-12, -1, -3, -4, -2};
+
+		assertEquals("testSearchMax1", 4, testSearchMax.searchMax(arr1));
+		assertEquals("testSearchMax2", -1, testSearchMax.searchMax(arr2));
+
+	}
+
+}
